dc951cd216e1e850b173f4e7a221d30b8632c3fd
zjjyyyk <791549989@qq.com>
2022-06-21 17:24:27 +0800

    完成创建操作系统，已初步构想操作系统结构

--- code.c
+++ code.c
@@ -21,7 +21,7 @@ typedef struct WORD{
 Space CreateOS();
 
 // 解析用户输入
-status CMD(char*);
+status CMD(Space, char*);
 
 // 内存分配算法
 Space AllocBoundTag(Space, int);
@@ -43,8 +43,13 @@ Space CreateOS(){
     fputc(621,fp);
     rewind(fp);
     Space head = (Space)malloc(sizeof(WORD));
-    head->
-
+    head->uplink = fp + 1024*64;  // 最多存储1024*128个内存碎片,需要1024*128*16Byte,即1024*64个&fp单位
+    head->tag = 0;
+    head->size = 1024*1024*99;  // 去掉1024*128个存储链表节点我们的操作系统一共有99M内存可以挥霍
+    head->llink = head->rlink = NULL;
+    fwrite(head,sizeof(WORD),1,fp);
+    fclose(fp);
+    return head;
 }
 
 
@@ -55,16 +60,16 @@ int main(){
     Space head;
     if ( (fp = fopen(OS_FILENAME, "r")) == NULL ) {
         head = CreateOS();
-        fp = fopen(OS_FILENAME,'w+');
+        fp = fopen(OS_FILENAME,'wb+');
     } else {
-        fp = fopen(OS_FILENAME,'w+');
+        fp = fopen(OS_FILENAME,'wb+');
         fread(head,sizeof(Space),1,fp);
     }
     char * commend;
     status flag = true;
     while(flag){
         gets(commend);
-        flag = CMD(commend);
+        flag = CMD(head, commend);
     }
     printf("成功退出操作系统");
     return 0;
